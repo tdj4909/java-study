892796337ddc4591400ee3daf0b7308f8f980bc7
tdj4909 <tdj4909@gmail.com>
2025-01-01 19:38:47 +0900

    Update code

--- src/collection/Member.java
+++ src/collection/Member.java
@@ -1,6 +1,6 @@
 package collection;
 
-public class Member {
+public class Member implements Comparable<Member>{
     private int memberId;
     private String memberName;
 
@@ -28,5 +28,29 @@ public class Member {
     @Override
     public String toString() {
         return memberName + " 회원님의 아이디는 " + memberId + "입니다";
+    }
+
+    @Override
+    public int hashCode() {
+        return memberId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof  Member){
+            Member member = (Member) obj;
+            if (this.memberId == member.memberId){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(Member member) {
+        return (this.memberId - member.memberId);
     }
 }
